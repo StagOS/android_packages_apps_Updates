PixelExperience/packages_apps_Updates

Refactor and format all code
Change-Id: I13f3a01e167f373b69987b3256098bbca92897f6

src/org/pixelexperience/ota/ExportUpdateService.java
@@ -26,9 +26,9 @@ import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Toast;
 
-import org.pixelexperience.ota.misc.FileUtils;
-
 import androidx.core.app.NotificationCompat;
+
+import org.pixelexperience.ota.misc.FileUtils;
 
 import java.io.File;
 import java.io.IOException;

StagOS/android_packages_apps_Updates

src/com/stag/ota/ExportUpdateService.java
@@ -26,9 +26,9 @@ import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.stag.ota.misc.FileUtils;
-
 import androidx.core.app.NotificationCompat;
+
+import com.stag.ota.misc.FileUtils;
 
 import java.io.File;
 import java.io.IOException;