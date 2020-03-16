PixelExperience/packages_apps_Updates

Finish up UI changes
Change-Id: I2c5b797242c2f213d38932d4d1f517792e5c21d3

src/org/pixelexperience/ota/ExtraCardView.java
@@ -1,0 +1,51 @@
+package org.pixelexperience.ota;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+
+public class ExtraCardView extends CardView {
+    TextView titleView;
+    TextView summaryView;
+    ImageView imageView;
+
+    public ExtraCardView(@NonNull Context context) {
+        this(context, null, 0);
+    }
+
+    public ExtraCardView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ExtraCardView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        addView(inflate(getContext(), R.layout.extra_card_view, null));
+        titleView = findViewById(R.id.card_title);
+        summaryView = findViewById(R.id.card_summary);
+        imageView = findViewById(R.id.card_image);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.extra_card_view,
+                0, 0);
+
+        try {
+            titleView.setText(a.getString(R.styleable.extra_card_view_title));
+            summaryView.setText(a.getString(R.styleable.extra_card_view_summary));
+            imageView.setImageDrawable(a.getDrawable(R.styleable.extra_card_view_image));
+        } finally {
+            a.recycle();
+        }
+    }
+    public void setSummary(String summary){
+        summaryView.setText(summary);
+    }
+}

StagOS/android_packages_apps_Updates

src/com/stag/ota/ExtraCardView.java
@@ -1,0 +1,51 @@
+package com.stag.ota;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+
+public class ExtraCardView extends CardView {
+    TextView titleView;
+    TextView summaryView;
+    ImageView imageView;
+
+    public ExtraCardView(@NonNull Context context) {
+        this(context, null, 0);
+    }
+
+    public ExtraCardView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ExtraCardView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        addView(inflate(getContext(), R.layout.extra_card_view, null));
+        titleView = findViewById(R.id.card_title);
+        summaryView = findViewById(R.id.card_summary);
+        imageView = findViewById(R.id.card_image);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.extra_card_view,
+                0, 0);
+
+        try {
+            titleView.setText(a.getString(R.styleable.extra_card_view_title));
+            summaryView.setText(a.getString(R.styleable.extra_card_view_summary));
+            imageView.setImageDrawable(a.getDrawable(R.styleable.extra_card_view_image));
+        } finally {
+            a.recycle();
+        }
+    }
+    public void setSummary(String summary){
+        summaryView.setText(summary);
+    }
+}